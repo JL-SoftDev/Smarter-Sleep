8fe192e832eca11f12ba8336ee633df5214ad8a9
Reiika <jmnewsome@yahoo.com>
2023-09-25 18:35:25 -0400

    -Changes to project structures

--- Frontend/Smarter_Sleep/smarter_sleep/lib/main.dart
+++ Frontend/Smarter_Sleep/smarter_sleep/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
-import 'HomeScreen.dart';
-//import 'SignInPage.dart';
+import 'app/screens/testingScreen.dart';
 
 void main() {
-  runApp(SmarterSleep());
+  runApp(const MaterialApp(title: "Smarter Sleep", home: TestingScreen()));
 }
-
+/*
 class SmarterSleep extends StatelessWidget {
   const SmarterSleep({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 217, 185, 255),
+            title: const Text('Smarter Sleep',
+                style: TextStyle(color: Color.fromARGB(255, 90, 68, 255)))),
+      ),
     );
   }
 }
+*/
