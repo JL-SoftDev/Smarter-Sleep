ef8014db8636c32e2b27f6e51047440b580d851b
rmack1020 <99986897+rmack1020@users.noreply.github.com>
2023-09-26 20:42:34 -0400

    Updated Home Screen
    Added main layout

--- Frontend/Smarter_Sleep/smarter_sleep/lib/app/screens/homeScreen.dart
+++ Frontend/Smarter_Sleep/smarter_sleep/lib/app/screens/homeScreen.dart
@@ -10,8 +10,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Home Screen")),
+    return Scaffold(
+      appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 36, 22, 65),
+            leading: const Icon(Icons.circle, color: Colors.amber),
+            leadingWidth: 40,
+            actions: [const Icon(Icons.settings)]),
+            bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Color.fromARGB(255, 39, 22, 74),
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.square), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.square), label: 'Shop'),
+              BottomNavigationBarItem(icon: Icon(Icons.square), label: 'Stats')
+            ],
+            selectedItemColor: Colors.amber,
+            unselectedItemColor: Colors.white,
+            ),     
+            body: ButtonGroup(),
+    );
+  }
+}
+class ButtonGroup extends StatelessWidget {
+  const ButtonGroup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          OutlinedButton(onPressed:(){print('Alarms');}, 
+                         child: Text('Alarms & Devices', style: TextStyle(color: Colors.white),),
+                         style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 134, 23, 159))),                     
+            ),
+          Spacer(),
+          OutlinedButton(onPressed: (){print('Sleep');},
+                         child: Text('Sleep', style: TextStyle(color: Colors.white)),
+                  
+                         style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 134, 23, 159)),
+                         shape: MaterialStatePropertyAll<CircleBorder>(CircleBorder()                                         
+                         ),
+                         fixedSize: MaterialStatePropertyAll<Size>(Size(90, 90)),
+                         )
+                         ),
+        ],
+      ),
     );
   }
 }
