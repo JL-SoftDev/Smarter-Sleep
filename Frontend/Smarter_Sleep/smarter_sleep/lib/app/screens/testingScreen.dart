0adf283cba1c9611c061f61770ebd6a6267a881d
cs_wtonk001 <wtonk001@odu.edu>
2023-10-19 16:41:37 -0400

    User creation and authentication using Cognito

--- Frontend/Smarter_Sleep/smarter_sleep/lib/app/screens/testingScreen.dart
+++ Frontend/Smarter_Sleep/smarter_sleep/lib/app/screens/testingScreen.dart
@@ -4,7 +4,6 @@ import 'package:smarter_sleep/app/screens/deviceConnectionScreen.dart';
 
 import 'homeScreen.dart';
 import 'inventoryScreen.dart';
-import 'loginScreen.dart';
 import 'settingsScreen.dart';
 import 'shopScreen.dart';
 import 'statsScreen.dart';
@@ -34,36 +33,27 @@ class _TestingScreenState extends State<TestingScreen> {
                 scrollDirection: Axis.horizontal,
                 children: [
                   TextButton(
-                      onPressed: _navigateToLoginScreen,
-                      child: Text("Login Screen")),
-                  TextButton(
                       onPressed: _navigateToHomeScreen,
                       child: Text("Home Screen")),
                   TextButton(
                       onPressed: _navigateToInventoryScreen,
                       child: Text("Inventory")),
                   //TextButton(
-                      //onPressed: _navigateToShopScreen, child: Text("Shop")),
+                  //onPressed: _navigateToShopScreen, child: Text("Shop")),
                   //TextButton(
-                      //onPressed: _navigateToStatsScreen, child: Text("Stats")),
+                  //onPressed: _navigateToStatsScreen, child: Text("Stats")),
                   TextButton(
                       onPressed: _navigateToSettingsScreen,
                       child: Text("Settings")),
                   TextButton(
                       onPressed: _navigateToDevicesScreen,
                       child: Text("Connected Devices")),
-                  
                 ]))
       ]),
     );
   }
 
   //Basic functions to navigate screens
-  void _navigateToLoginScreen() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const LoginScreen()));
-  }
-
   void _navigateToHomeScreen() {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => const HomeScreen()));
@@ -88,8 +78,11 @@ class _TestingScreenState extends State<TestingScreen> {
     Navigator.push(context,
         MaterialPageRoute(builder: (context) => const SettingsScreen()));
   }
+
   void _navigateToDevicesScreen() {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => const DeviceConnectionsScreen()));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const DeviceConnectionsScreen()));
   }
 }
