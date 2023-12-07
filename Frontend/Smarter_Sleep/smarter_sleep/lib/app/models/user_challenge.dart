600525ce0dbe1e16475f318345f1c7442babbf5b
cs_wtonk001 <wtonk001@odu.edu>
2023-12-07 11:05:39 -0500

    Get challenge progress on home screen

--- Frontend/Smarter_Sleep/smarter_sleep/lib/app/models/user_challenge.dart
+++ Frontend/Smarter_Sleep/smarter_sleep/lib/app/models/user_challenge.dart
@@ -1,24 +1,33 @@
 class UserChallenge {
   final String challengeName;
-  final DateTime startDate;
-  final DateTime expireDate;
+  final String? challengeDesc;
+  final DateTime? startDate;
+  final DateTime? expireDate;
   final bool userSelected;
   final double completionPercentage;
+  final int numCompleted;
+  final int numTargetted;
 
   UserChallenge({
     required this.challengeName,
+    this.challengeDesc,
     required this.startDate,
-    required this.expireDate,
+    this.expireDate,
     required this.userSelected,
     required this.completionPercentage,
+    required this.numCompleted,
+    required this.numTargetted,
   });
   factory UserChallenge.fromJson(Map<String, dynamic> json) {
     return UserChallenge(
-      challengeName: "Default",
+      challengeName: json['challengeName'],
+      challengeDesc: json['challengeDescription'],
       startDate: DateTime.parse(json['startDate']),
       expireDate: DateTime.parse(json['expireDate']),
       userSelected: json['userSelected'],
       completionPercentage: json['completionPercentage'],
+      numCompleted: json['completed'],
+      numTargetted: json['goal'],
     );
   }
 }
